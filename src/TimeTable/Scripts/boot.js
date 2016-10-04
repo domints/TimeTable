3e04eb82e78302d869da9635323296eb6e828802
Dominik Szymański <dominik.szymanski@skmgroup.pl>
2016-10-04 11:16:04 +0200

    Revert "Revert back"
    This reverts commit a9642e167175907b7cbc492b2dd5bbf130b79a10.

--- src/TimeTable/Scripts/boot.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
-var browser_1 = require("angular2/platform/browser");
-var http_1 = require("angular2/http");
-var app_1 = require("./app");
-var auth_service_1 = require("./services/auth.service");
-browser_1.bootstrap(app_1.AppComponent, [auth_service_1.AuthService, http_1.HTTP_PROVIDERS]);
